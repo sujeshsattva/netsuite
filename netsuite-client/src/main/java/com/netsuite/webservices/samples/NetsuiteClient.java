21964dcb3f2e0bd71b9e74ce44c2d4a00831a03a
sujeshsattva <78430201+sujeshsattva@users.noreply.github.com>
2021-02-10 16:05:38 +0530

    code change

--- netsuite-client/src/main/java/com/netsuite/webservices/samples/NetsuiteClient.java
+++ netsuite-client/src/main/java/com/netsuite/webservices/samples/NetsuiteClient.java
@@ -1,6 +1,8 @@
 package com.netsuite.webservices.samples;
 
 import com.netsuite.suitetalk.client.v2020_1.WsClient;
+import com.netsuite.suitetalk.proxy.v2020_1.platform.core.types.GetAllRecordType;
+
 import org.apache.axis.AxisFault;
 
 import java.io.IOException;
@@ -10,34 +12,33 @@ import static com.netsuite.webservices.samples.Messages.*;
 import static com.netsuite.webservices.samples.utils.PrintUtils.printError;
 
 /**
- * <p>Fully functional, command-line driven application that illustrates how to connect to the NetSuite web services
- * and invoke operations.</p>
- * <p>Please see the README on how to compile and run. Note that the {@code nsclient.properties} file must exist
- * in the installed root directory for this application to run.</p>
- * <p>© 2019 NetSuite Inc. All rights reserved.</p>
+ * <p>
+ * Fully functional, command-line driven application that illustrates how to
+ * connect to the NetSuite web services and invoke operations.
+ * </p>
+ * <p>
+ * Please see the README on how to compile and run. Note that the
+ * {@code nsclient.properties} file must exist in the installed root directory
+ * for this application to run.
+ * </p>
+ * <p>
+ * © 2019 NetSuite Inc. All rights reserved.
+ * </p>
  */
 public class NetsuiteClient {
 
-    public static void main(String[] args) {
-        WsClient client = null;
-        try {
-            client = WsClientFactory.getWsClient(new Properties(), null);
-            new NetsuiteOperations(client).run();
-        } catch (MalformedURLException e) {
-            printError(INVALID_WS_URL, e.getMessage());
-            System.exit(2);
-        } catch (AxisFault e) {
-        	printError(ERROR_OCCURRED, e.getFaultDetails());
-            
+	public static void main(String[] args) {
+		WsClient client = null;
+		try {
+			client = WsClientFactory.getWsClient(new Properties(), null);
+			new NetsuiteOperations(client).run(GetAllRecordType.currency);
+		} catch (AxisFault e) {
+            printError(ERROR_OCCURRED, e.getFaultString());
             System.exit(3);
-        } catch (IOException e) {
-            printError(WRONG_PROPERTIES_FILE, e.getMessage());
-            System.exit(1);
-        }catch(Exception e) {
-        	printError(ERROR,e.getStackTrace());
-        	printError(ERROR_OCCURRED, e.getMessage());
-        	System.exit(0);
-        }
-        
-    }
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
